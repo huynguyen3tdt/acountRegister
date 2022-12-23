b02f9863855a36aa31b36496108205a86d4a4295
huynguyen3tdt <huynguyen3@cqtdt.asia>
2022-12-23 16:11:31 +0700

    build component register

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl, Validator, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css']
+})
+export class AccountComponent implements OnInit{
+  rfRegister: FormGroup= new FormGroup({});
+
+  ngOnInit() {
+    this.rfRegister = new FormGroup({ //inputSurName thuộc vào rfRegister
+      inputSurname: new FormControl('huy', Validators.required),
+      inputName: new FormControl('',Validators.required),
+      inputEmail: new FormControl('',Validators.required)
+    })
+
+    console.log(this.rfRegister)
+  }
+  onSubmit(rf: object){
+    console.log(rf)
+  }
+
+}
+// form control có các tham số (value, validate, validator)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
